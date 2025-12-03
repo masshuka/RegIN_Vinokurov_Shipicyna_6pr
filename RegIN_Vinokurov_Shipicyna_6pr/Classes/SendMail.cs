650c064c0f63e17400c01c8c813f7331389d4fc0
Masshh <msipicyna96@gmail.com>
2025-12-03 19:11:42 +0500

    Разработанная страница Recovery. Шипицына.

---
+++ RegIN_Vinokurov_Shipicyna_6pr/Classes/SendMail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegIN_Vinokurov_Shipicyna_6pr.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("msvlls@yandex.ru", "vitfdmtkwzsmssjp"),
+                EnableSsl = true,
+            };
+
+            smtpClient.Send("msvlls@yandex.ru", To, "RegIN_6pr", Message);
+        }
+    }
+}
--- RegIN_Vinokurov_Shipicyna_6pr/MainWindow.xaml.cs
+++ RegIN_Vinokurov_Shipicyna_6pr/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using RegIN_Vinokurov_Shipicyna_6pr.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +23,36 @@ namespace RegIN_Vinokurov_Shipicyna_6pr
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public User UserLogIn = new User();
+
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
+            OpenPage(new Pages.Login());
+        }
+
+        public void OpenPage(Page page)
+        {
+            DoubleAnimation StartAnimation = new DoubleAnimation();
+            StartAnimation.From = 1;
+            StartAnimation.To = 0;
+            StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+
+            StartAnimation.Completed += delegate
+            {
+                frame.Navigate(page);
+
+                DoubleAnimation EndAnimation = new DoubleAnimation();
+                EndAnimation.From = 0;
+                EndAnimation.To = 1;
+                EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+
+                frame.BeginAnimation(Frame.OpacityProperty, EndAnimation);
+            };
+
+            frame.BeginAnimation(Frame.OpacityProperty, StartAnimation);
         }
     }
 }
