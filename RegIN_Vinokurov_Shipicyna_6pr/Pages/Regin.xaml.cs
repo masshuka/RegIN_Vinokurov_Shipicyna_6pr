ae2a4e794fb8a3b880693a806e4c07b98d3b052a
Masshh <msipicyna96@gmail.com>
2025-12-03 21:12:13 +0500

    Программирование страницы Regin. Шипицына.

--- RegIN_Vinokurov_Shipicyna_6pr/Pages/Regin.xaml.cs
+++ RegIN_Vinokurov_Shipicyna_6pr/Pages/Regin.xaml.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿using Aspose.Imaging;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +26,283 @@ namespace RegIN_Vinokurov_Shipicyna_6pr.Pages
     /// </summary>
     public partial class Regin : Page
     {
+        /// <summary>
+        /// Файловый диалог
+        /// </summary>
+        OpenFileDialog FileDialogImage = new OpenFileDialog();
+
+        /// <summary>
+        /// Проверка на ввод логина
+        /// </summary>
+        bool BCorrectLogin = false;
+
+        /// <summary>
+        /// Проверка на ввод пароля
+        /// </summary>
+        bool BCorrectPassword = false;
+
+        /// <summary>
+        /// Проверка на подтверждение пароля
+        /// </summary>
+        bool BCorrectConfirmPassword = false;
+
+        /// <summary>
+        /// Проверка на указания изображения
+        /// </summary>
+        bool BSetImages = false;
+
         public Regin()
         {
             InitializeComponent();
+            MainWindow.mainWindow.UserLogIn.HandlerCorrectLogin += CorrectLogin;
+            MainWindow.mainWindow.UserLogIn.HandlerInCorrectLogin += InCorrectLogin;
+            FileDialogImage.Filter = "PNG (*.png)|*.png|JPG (*.jpg)|*.jpg";
+            FileDialogImage.RestoreDirectory = true;
+            FileDialogImage.Title = "Choose a photo for your avatar";
+        }
+
+        /// <summary>
+        /// Функция правильно введённого логина
+        /// </summary>
+        private void CorrectLogin()
+        {
+            SetNotification("Login already in use", Brushes.Red);
+            BCorrectLogin = false;
+        }
+
+        /// <summary>
+        /// Функция не правильно введённого логина
+        /// </summary>
+        private void InCorrectLogin() =>
+            SetNotification("", Brushes.Black);
+
+
+        /// <summary>
+        /// Метод ввода логина
+        /// </summary>
+        private void SetLogin(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                SetLogin();
+        }
+
+        /// <summary>
+        /// Метод ввода логина
+        /// </summary>
+        private void SetLogin(object sender, System.Windows.RoutedEventArgs e) =>
+            SetLogin();
+
+        /// <summary>
+        /// Метод ввода логина
+        /// </summary>
+        public void SetLogin()
+        {
+            Regex regex = new Regex(@"([a-zA-Z0-9._-]{4,})@[a-zA-Z0-9._-]{2,}\.[a-zA-Z0-9._-]{2,})");
+            BCorrectLogin = regex.IsMatch(TbLogin.Text);
+
+            if (regex.IsMatch(TbLogin.Text) == true)
+            {
+                SetNotification("", Brushes.Black);
+                MainWindow.mainWindow.UserLogIn.GetUserLogin(TbLogin.Text);
+            }
+            else
+                SetNotification("Invalid login", Brushes.Red);
+
+            OnRegin();
+        }
+
+        #region SetPassword
+        /// <summary>
+        /// Метод ввода пароля
+        /// </summary>
+        private void SetPassword(object sender, System.Windows.RoutedEventArgs e) =>
+            SetPassword();
+
+        /// <summary>
+        /// Метод ввода пароля
+        /// </summary>
+        private void SetPassword(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                SetPassword();
+        }
+
+        /// <summary>
+        /// Метод ввода пароля
+        /// </summary>
+        public void SetPassword()
+        {
+            Regex regex = new Regex(@"(?=.*[0-9])(?=.*[!@#$%^&?*\-_=])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&?*\-_=]{10,}");
+            // (?=.*[0-9]) - строка содержит хотя бы одно число;
+            // (?=.*[!@#$%^&?*\-_=]) - строка содержит хотя бы один спецсимвол;
+            // (?=.*[a-z]) - строка содержит хотя бы одну латинскую букву в нижнем регистре;
+            // (?=.*[A-Z]) - строка содержит хотя бы одну латинскую букву в верхнем регистре;
+            // [0-9a-zA-Z!@#$%^&?*\-_=]{10,} - строка состоит не менее, чем из 10 вышеупомянутых символов.
+
+            BCorrectPassword = regex.IsMatch(PbPassword.Password);
+
+            if (regex.IsMatch(PbPassword.Password) == true)
+            {
+                SetNotification("", Brushes.Black);
+                if (PbConfirmPassword.Password.Length > 0)
+                    ConfirmPassword(true);
+                OnRegin();
+            }
+            else
+                SetNotification("Invalid password", Brushes.Red);
+        }
+        #endregion
+
+        #region SetConfirmPassword
+        /// <summary>
+        /// Метод повторного ввода пароля
+        /// </summary>
+        private void ConfirmPassword(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                ConfirmPassword();
+        }
+
+        /// <summary>
+        /// Метод повторного ввода пароля
+        /// </summary>
+        private void ConfirmPassword(object sender, System.Windows.RoutedEventArgs e) =>
+            ConfirmPassword();
+
+        /// <summary>
+        /// Метод повторного ввода пароля
+        /// </summary>
+        public void ConfirmPassword(bool Pass = false)
+        {
+            BCorrectConfirmPassword = PbConfirmPassword.Password == PbPassword.Password;
+
+            if (PbConfirmPassword.Password != PbPassword.Password)
+                SetNotification("Passwords do not match", Brushes.Red);
+            else
+            {
+                SetNotification("", Brushes.Black);
+                if (!Pass)
+                    SetPassword();
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Метод регистрации
+        /// </summary>
+        void OnBegin()
+        {
+            if (!BCorrectLogin)
+                return;
+            if (TbName.Text.Length == 0)
+                return;
+            if (!BCorrectPassword)
+                return;
+            if (!BCorrectConfirmPassword)
+                return;
+
+            MainWindow.mainWindow.UserLogIn.Login = TbLogin.Text;
+            MainWindow.mainWindow.UserLogIn.Password = PbPassword.Password;
+            MainWindow.mainWindow.UserLogIn.Name = TbName.Text;
+
+            if (BSetImages)
+                MainWindow.mainWindow.UserLogIn.Image = File.ReadAllBytes(Directory.GetCurrentDirectory() + @"\IUser.jpg");
+
+            MainWindow.mainWindow.UserLogIn.DateUpdate = DateTime.Now;
+            MainWindow.mainWindow.UserLogIn.DateCreate = DateTime.Now;
+            MainWindow.mainWindow.OpenPage(new Confirmation(Confirmation.TypeConfirmation.Regin));
+        }
+
+        /// <summary>
+        /// Ввод букв
+        /// </summary>
+        private void SetName(object sender, TextCompositionEventArgs e)
+        {
+            // Проверяем что символ относится к категории букв
+            e.Handled = !(Char.IsLetter(e.Text, 0));
+        }
+
+        public void SetNotification(string Message, SolidColorBrush _Color)
+        {
+            Username.Content = Message;
+            Username.Foreground = _Color;
+        }
+
+
+        private void SelectImage(object sender, MouseButtonEventArgs e)
+        {
+            if (FileDialogImage.ShowDialog() == true)
+            {
+                using (Imaging.Image image = Imaging.Image.Load(FileDialogImage.FileName))
+                {
+                    int NewWidth = 0;
+                    int NewHeight = 0;
+
+                    if (image.Width > image.Height)
+                    {
+                        NewWidth = (int)(image.Width * (256f / image.Height));
+                        NewHeight = 256;
+                    }
+                    else
+                    {
+                        NewWidth = 256;
+                        NewHeight = (int)(image.Height * (256f / image.Width));
+                    }
+
+                    image.Resize(NewWidth, NewHeight);
+                    image.Save("IUser.jpg");
+                }
+
+                using (Imaging.RasterImage rasterImage = (Imaging.RasterImage)Imaging.Image.Load("IUser.jpg"))
+                {
+                    if (!rasterImage.IsCached)
+                    {
+                        rasterImage.CacheData();
+                    }
+                    int X = 0;
+                    int Width = 256;
+                    int Y = 0;
+                    int Height = 256;
+
+                    if (rasterImage.Width > rasterImage.Height)
+                        X = (int)((rasterImage.Width - 256f) / 2);
+                    else
+                        Y = (int)((rasterImage.Height - 256f) / 2);
+
+                    Imaging.Rectangle rectangle = new Imaging.Rectangle(X, Y, Width, Height);
+                    rasterImage.Crop(rectangle);
+
+                    rasterImage.Save("IUser.jpg");
+                }
+
+                DoubleAnimation StartAnimation = new DoubleAnimation();
+                StartAnimation.From = 1;
+                StartAnimation.To = 0;
+                StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+
+                StartAnimation.Completed += delegate
+                {
+                    IUser.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + @"\IUser.jpg"));
+                    DoubleAnimation EndAnimation = new DoubleAnimation();
+                    EndAnimation.From = 0;
+                    EndAnimation.To = 1;
+                    EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                    IUser.BeginAnimation(Image.OpacityProperty, EndAnimation);
+                };
+
+                IUser.BeginAnimation(Image.OpacityProperty, StartAnimation);
+                BSetImages = true;
+            }
+            else
+                BSetImages = false;
+        }
+
+        /// <summary>
+        /// Метод перехода на авторизацию
+        /// </summary>
+        private void OpenLogin(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.OpenPage(new Login());
         }
     }
 }
